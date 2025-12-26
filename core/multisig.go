99ab810dfb491edd29073600f103fec551d1c1f5
hc172808 <hc172808@gmail.com>
2025-12-26 13:18:17 -0400

    Create multisig.go

---
+++ core/multisig.go
@@ -0,0 +1,11 @@
+{
+  "treasury": {
+    "owners": [
+      "GYDS1FounderAddr",
+      "GYDS1AdminAddr",
+      "GYDS1BackupAddr"
+    ],
+    "required_signatures": 2,
+    "timelock_seconds": 86400
+  }
+}
