75aeab63c1e5a82fa6e9752b283b0144492b558b
hc172808 <hc172808@gmail.com>
2026-01-03 15:02:36 -0400

    Update staking.go

--- consensus/pos/staking.go
+++ consensus/pos/staking.go
@@ -12,14 +12,11 @@ func (e *Engine) ApplyStake(tx core.Transaction, acc *core.Account) error {
 	if tx.Amount < MinValidatorStake {
 		return ErrStakeTooSmall
 	}
-
 	if acc.Balance["GYDS"] < tx.Amount {
 		return ErrInsufficientFunds
 	}
-
 	acc.Balance["GYDS"] -= tx.Amount
 	acc.Staked += tx.Amount
-
 	e.addOrIncreaseValidator(string(tx.From), tx.Amount)
 	return nil
 }
@@ -28,14 +25,11 @@ func (e *Engine) ApplyUnstake(tx core.Transaction, acc *core.Account) error {
 	if acc.Staked < tx.Amount {
 		return ErrInsufficientStake
 	}
-
 	acc.Staked -= tx.Amount
 	acc.Unbonding += tx.Amount
-
 	v := e.validator(string(tx.From))
 	if v != nil {
 		v.UnbondingHeight = e.State.Height + UnbondingPeriod
 	}
-
 	return nil
 }
