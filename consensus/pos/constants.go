3ef6d39b102e61783116e7c15b53f7cca1231a2e
hc172808 <hc172808@gmail.com>
2026-01-03 15:24:58 -0400

    Update constants.go

--- consensus/pos/constants.go
+++ consensus/pos/constants.go
@@ -1,7 +1,7 @@
 package pos
 
 const (
-	MinValidatorStake = 1_000_000_000 // 10 GYDS
-	UnbondingPeriod   = 10            // in blocks
-	BlockInterval     = 5             // seconds between blocks (demo)
+	MinValidatorStake = 1_000_000_000
+	UnbondingPeriod   = 10
+	BlockInterval     = 5
 )
