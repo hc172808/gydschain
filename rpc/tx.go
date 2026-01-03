fcea3b4bcc5e8b99bae9fcf3d1693ac4d5445bdc
hc172808 <hc172808@gmail.com>
2026-01-03 14:54:14 -0400

    Update tx.go

--- rpc/tx.go
+++ rpc/tx.go
@@ -1,10 +1,22 @@
+package rpc
+
+import "github.com/hc172808/gydschain/core"
+
 func (s *Server) SendRawTx(tx core.Transaction) error {
 	acc := s.state.Account(tx.From)
 
 	if tx.Nonce != acc.Nonce {
-		return ErrBadNonce
+		return &core.ErrString{"bad nonce"}
 	}
 
 	acc.Nonce++
-	return core.ApplyTx(tx, acc, s.engine)
+
+	switch tx.Type {
+	case core.TxStake:
+		return s.engine.ApplyStake(tx, acc)
+	case core.TxUnstake:
+		return s.engine.ApplyUnstake(tx, acc)
+	default:
+		return nil
+	}
 }
