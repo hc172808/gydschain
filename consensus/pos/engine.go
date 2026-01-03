11f9daf06e346fdb9d88b965747add5bb3eaeccc
hc172808 <hc172808@gmail.com>
2026-01-03 14:45:47 -0400

    Update engine.go

--- consensus/pos/engine.go
+++ consensus/pos/engine.go
@@ -2,19 +2,16 @@ package pos
 
 import "github.com/hc172808/gydschain/core"
 
-// Engine handles PoS consensus
 type Engine struct {
 	State *State
 }
 
-// NewEngine creates an empty PoS engine
 func NewEngine() *Engine {
 	return &Engine{
 		State: NewState(),
 	}
 }
 
-// NewEngineFromGenesis initializes engine from genesis validators
 func NewEngineFromGenesis(g *core.Genesis) *Engine {
 	state := NewState()
 
@@ -29,7 +26,6 @@ func NewEngineFromGenesis(g *core.Genesis) *Engine {
 	return &Engine{State: state}
 }
 
-// NewState creates empty PoS state
 func NewState() *State {
 	return &State{}
 }
