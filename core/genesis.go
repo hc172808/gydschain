46cea067a9c841fd0d2607d9c1a03b681b3e5640
hc172808 <hc172808@gmail.com>
2026-01-03 13:58:34 -0400

    Create genesis.go

---
+++ core/genesis.go
@@ -0,0 +1,34 @@
+package core
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
+
+type Genesis struct {
+	ChainID     string             `json:"chain_id"`
+	GenesisTime time.Time          `json:"genesis_time"`
+	Assets      []Asset            `json:"assets"`
+	Balances    map[Address]map[string]uint64 `json:"balances"`
+	Validators  []GenesisValidator `json:"validators"`
+}
+
+type GenesisValidator struct {
+	Address Address `json:"address"`
+	Stake   uint64  `json:"stake"`
+}
+
+func LoadGenesis(path string) (*Genesis, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var g Genesis
+	if err := json.Unmarshal(data, &g); err != nil {
+		return nil, err
+	}
+
+	return &g, nil
+}
