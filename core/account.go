9db6e4d68bbf844d4c2127494bac32593b417cf4
hc172808 <hc172808@gmail.com>
2026-01-03 15:58:43 -0400

    Update account.go

--- core/account.go
+++ core/account.go
@@ -1,10 +1,33 @@
 package core
 
-type Account struct {
-	Address Address
-	Nonce   uint64
-	Balance map[string]uint64
+import (
+    "errors"
+    "github.com/hc172808/gydschain/types"
+)
 
-	Staked    uint64
-	Unbonding uint64
+type Ledger struct {
+    Accounts map[types.Address]*types.Account
+}
+
+func NewLedger() *Ledger {
+    return &Ledger{
+        Accounts: make(map[types.Address]*types.Account),
+    }
+}
+
+func (l *Ledger) GetAccount(addr types.Address) (*types.Account, error) {
+    acc, ok := l.Accounts[addr]
+    if !ok {
+        return nil, errors.New("account not found")
+    }
+    return acc, nil
+}
+
+func (l *Ledger) UpdateBalance(addr types.Address, amount uint64) error {
+    acc, err := l.GetAccount(addr)
+    if err != nil {
+        return err
+    }
+    acc.Balance += amount
+    return nil
 }
