e617e9083d61070a0db22bdcdfec92586ed5976d
hc172808 <hc172808@gmail.com>
2026-01-03 15:03:45 -0400

    Update tx.go

--- rpc/tx.go
+++ rpc/tx.go
@@ -1,14 +1,36 @@
 package rpc
 
-import "github.com/hc172808/gydschain/core"
+import (
+	"github.com/hc172808/gydschain/core"
+	"github.com/hc172808/gydschain/consensus/pos"
+)
+
+type Server struct {
+	engine *pos.Engine
+	state  *State
+}
+
+type State struct {
+	Accounts map[core.Address]*core.Account
+}
+
+func (s *Server) Account(addr core.Address) *core.Account {
+	acc, ok := s.state.Accounts[addr]
+	if !ok {
+		acc = &core.Account{
+			Address: addr,
+			Balance: map[string]uint64{"GYDS": 0},
+		}
+		s.state.Accounts[addr] = acc
+	}
+	return acc
+}
 
 func (s *Server) SendRawTx(tx core.Transaction) error {
-	acc := s.state.Account(tx.From)
-
+	acc := s.Account(tx.From)
 	if tx.Nonce != acc.Nonce {
 		return &core.ErrString{"bad nonce"}
 	}
-
 	acc.Nonce++
 
 	switch tx.Type {
