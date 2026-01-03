b52c1382576c510b5a9393b821330c43eda3427d
hc172808 <hc172808@gmail.com>
2026-01-03 15:26:05 -0400

    Update engine.go

--- consensus/pos/engine.go
+++ consensus/pos/engine.go
@@ -27,3 +27,11 @@ func NewEngineFromGenesis(g *core.Genesis) *Engine {
 func NewState() *State {
 	return &State{}
 }
+
+func (s *State) Proposer() *Validator {
+	if len(s.Validators) == 0 {
+		return nil
+	}
+	index := int(s.Height % uint64(len(s.Validators)))
+	return s.Validators[index]
+}
