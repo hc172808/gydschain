04ca951e3f4f5f81a19213938d22353b21d27028
hc172808 <hc172808@gmail.com>
2026-01-03 15:00:55 -0400

    Update validator.go

--- consensus/pos/validator.go
+++ consensus/pos/validator.go
@@ -1,9 +1,9 @@
 package pos
 
 type Validator struct {
-	Address  string
-	Stake    uint64
-	Power    uint64
-	Slashed  bool
+	Address         string
+	Stake           uint64
+	Power           uint64
+	Slashed         bool
 	UnbondingHeight uint64
 }
