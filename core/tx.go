3e5cbb3041b3aebbbf5e08a31c3752b186310295
hc172808 <hc172808@gmail.com>
2026-01-03 14:09:30 -0400

    Update tx.go

--- core/tx.go
+++ core/tx.go
@@ -1,11 +1,18 @@
 package core
 
+type TxType uint8
+
+const (
+	TxTransfer TxType = iota
+	TxStake
+	TxUnstake
+)
+
 type Transaction struct {
-    From      Address
-    To        Address
-    AssetID  string
-    Amount   uint64
-    Fee      uint64
-    Nonce    uint64
-    Signature []byte
+	Type      TxType
+	From      Address
+	Amount    uint64
+	Fee       uint64
+	Nonce     uint64
+	Signature []byte
 }
