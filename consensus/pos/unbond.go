dcb67903424ce6f998e7ea3d299ff6839247b938
hc172808 <hc172808@gmail.com>
2026-01-03 14:21:07 -0400

    Update unbond.go

--- consensus/pos/unbond.go
+++ consensus/pos/unbond.go
@@ -1,11 +1,16 @@
 package pos
 
-func (e *Engine) FinalizeUnbonding(accounts map[string]*Account) {
+import "github.com/hc172808/gydschain/core"
+
+// FinalizeUnbonding releases funds after unbonding period
+func (e *Engine) FinalizeUnbonding(accounts map[string]*core.Account) {
 	for _, v := range e.State.Validators {
 		if v.UnbondingHeight > 0 && e.State.Height >= v.UnbondingHeight {
-			acc := accounts[string(v.Address)]
-			acc.Balance["GYDS"] += acc.Unbonding
-			acc.Unbonding = 0
+			acc := accounts[v.Address]
+			if acc != nil {
+				acc.Balance["GYDS"] += acc.Unbonding
+				acc.Unbonding = 0
+			}
 			v.UnbondingHeight = 0
 		}
 	}
