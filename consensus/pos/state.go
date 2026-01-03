ddd0240ed9c4f681013cd4f8146aae0336556188
hc172808 <hc172808@gmail.com>
2026-01-03 14:18:51 -0400

    Update state.go

--- consensus/pos/state.go
+++ consensus/pos/state.go
@@ -1,13 +1,7 @@
 package pos
 
-import "github.com/hc172808/gydschain/core"
-
 type State struct {
 	Validators []*Validator
 	Height     uint64
 	Round      uint64
-}
-
-func NewState() *State {
-	return &State{}
 }
