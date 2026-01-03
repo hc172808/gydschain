0362293d9592f67825ac6804912d6bbaa6672106
hc172808 <hc172808@gmail.com>
2026-01-03 14:43:49 -0400

    Create types.go

---
+++ core/types.go
@@ -0,0 +1,51 @@
+package core
+
+type Address string
+
+type Asset struct {
+	ID       string
+	Name     string
+	Symbol   string
+	Decimals uint8
+	Native   bool
+}
+
+type Block struct {
+	Height    uint64
+	Timestamp int64
+	Proposer  Address
+}
+
+type Account struct {
+	Address   Address
+	Nonce     uint64
+	Balance   map[string]uint64
+	Staked    uint64
+	Unbonding uint64
+}
+
+type TxType uint8
+
+const (
+	TxTransfer TxType = iota
+	TxStake
+	TxUnstake
+)
+
+type Transaction struct {
+	Type      TxType
+	From      Address
+	Amount    uint64
+	Fee       uint64
+	Nonce     uint64
+	Signature []byte
+}
+
+// Simple error wrapper
+type ErrString struct {
+	S string
+}
+
+func (e *ErrString) Error() string {
+	return e.S
+}
