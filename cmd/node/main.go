c13c3c1802aef2478925891b8a6dc1520298f686
hc172808 <hc172808@gmail.com>
2026-01-03 13:00:43 -0400

    Update main.go

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,1 +1,17 @@
 
+package main
+
+import (
+    "github.com/hc172808/gydschain/consensus/pos"
+    "github.com/hc172808/gydschain/rpc"
+    "github.com/hc172808/gydschain/utils"
+)
+
+func main() {
+    utils.Info("Starting GYDS node")
+
+    _ = pos.NewEngine()
+
+    server := rpc.New(":8545")
+    server.Start()
+}
